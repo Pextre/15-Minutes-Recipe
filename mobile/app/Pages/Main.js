e0950bc3c48f28b61c2f6b529cff8afe6c7a2bf3
lucasdasilvalima <lima.1996@alunos.utfpr.edu.br>
2020-03-28 15:46:19 -0300

    Manutenção para código limpo

--- mobile/app/Pages/Main.js
+++ mobile/app/Pages/Main.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View, TextInput, Text, FlatList } from 'react-native';
 import Recipe from "../components/Recipe";
 import api from '../services/api';
@@ -6,54 +6,34 @@ import api from '../services/api';
 export default function Main({ navigation }) {
     const STATUS_SUCESS = 200
     const [recipes, setRecipes] = useState([]);
-    const [newRecipes, setNewRecipes] = useState([]);
     const [recipeText, setRecipeText] = useState("");
-    const [isFetching, setIsFetching] = useState(true);
-    const [pageCounter, setPageCounter] = useState(1);
-
-    /*
-    */
-
-    async function loadRecipes() {
-        if (isFetching) {
-            return ;
-        }
-    }
+    const [recipesInfo, setRecipesInfo] = useState({ page: "1", pages: 0 });
 
     useEffect(() => {
-        async function loadFirstRecipes() {
+        async function loadRecipes(pageNumber = 1) {
 
-            const response = await api.get('/recipes', { params: { page: pageCounter } });
-            const responseRecipes = response.data.docs;
-            if (response.status === STATUS_SUCESS) {
-                setRecipes(responseRecipes);
-                setIsFetching(false);
-                setPageCounter(2);
+            const response = await api.get('/recipes', { params: { page: pageNumber } });
+            const { status, data } = response;
+            const { docs, page, pages } = data;
+            if (status === STATUS_SUCESS) {
+                setRecipes([...recipes, ...docs]);
+                setRecipesInfo({ page, pages });
             }
         }
-        loadFirstRecipes();
+        loadRecipes();
     }, []);
 
-    useEffect(() => {
-        async function loadMoreRecipes() {
-            if(isFetching){
-                const response = await api.get('/recipes', { params: { page: pageCounter } });
-                const responseRecipes = response.data.docs;
-                if (response.status === STATUS_SUCESS && recipes.length) {
-                    setNewRecipes(responseRecipes);
-                    setIsFetching(false);
-                    setPageCounter(1+pageCounter)
-                }
-            }
-        }
-        loadMoreRecipes();
-    }, [isFetching]);
 
-    useEffect(() => {
-        if(newRecipes.length){
-            setRecipes([...recipes,...newRecipes]);
-        } 
-    }, [newRecipes]);
+    function loadMoreRecipes() {
+        const { page, pages } = recipesInfo;
+        if (Number(page) === pages)
+            return
+        loadRecipes(Number(page) + 1);
+    }
+
+    function renderItem({ item }) {
+        return <Item recipe={item} />;
+    }
 
     function Item({ recipe }) {
         return (<Recipe key={recipe._id} recipe={recipe} navigation={navigation} />);
@@ -62,39 +42,25 @@ export default function Main({ navigation }) {
     function NoItems() {
         return (<Text style={styles.noRecipe}>Não existe nenhuma receita ainda...</Text>);
     }
-
     return (
+
         <View style={styles.container} >
             <TextInput
                 style={styles.headerInput}
-                onChangeText={text => setRecipeText(text)}
+                onChangeText={setRecipeText}
                 placeholder="Pesquise sua receita aqui..."
                 placeholderTextColor="#000"
-                autoCorrect={false}
+                autoCorrect={true}
                 ref={input => { textInput = input }}
             >
             </TextInput>
-            {/* <ScrollView style={styles.scrollContainer}>
-                {recipes.length === 0 ?
-                    <Text style={styles.noRecipe}>Não existe nenhuma receita ainda...</Text>
-                    :
-                    recipes.map((recipe) => (
-
-                        <Recipe key={recipe._id} recipe={recipe} navigation={navigation} />
-
-                    ))
-                }
-            </ScrollView> */}
-            <FlatList style={styles.scrollContainer}
+            <FlatList
+                style={styles.scrollContainer}
                 data={recipes}
-                ListEmptyComponent={() => <NoItems />}
-                renderItem={({ item }) => <Item recipe={item} />}
                 keyExtractor={item => item._id}
-                onEndReached={() => {
-                    if (!isFetching) {
-                        setIsFetching(true)
-                    }
-                }}
+                renderItem={renderItem}
+                ListEmptyComponent={() => <NoItems />}
+                onEndReached={loadMoreRecipes}
                 onEndReachedThreshold={0.9}
             />
         </View>
