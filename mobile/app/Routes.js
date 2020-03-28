e14a3386322b3dcc5c1df93b597cff5671fcad93
lucasdasilvalima <lima.1996@alunos.utfpr.edu.br>
2020-03-28 15:45:30 -0300

    Definição das rotas de navegação

--- mobile/app/Routes.js
+++ mobile/app/Routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import {TouchableOpacity,Image} from 'react-native';
+import { TouchableOpacity } from 'react-native';
 import { MaterialIcons } from "@expo/vector-icons";
 
 
@@ -12,48 +12,48 @@ import InfoPage from "./Pages/InfoPage";
 
 
 const Routes = createStackNavigator({
-        Main: {
-            screen: Main,
-            navigationOptions: ({ navigate, navigation }) => ({
-                title:"15-minutes recipes",
-                headerRight:()=> (
-                  <TouchableOpacity
+    Main: {
+        screen: Main,
+        navigationOptions: ({ navigation }) => ({
+            title: "15-minutes recipes",
+            headerRight: () => (
+                <TouchableOpacity
                     onPress={() => navigation.navigate("InfoPage")}
-                  >
+                >
                     <MaterialIcons name="info-outline" size={36} color="#000" />
-                  </TouchableOpacity>
-                )
-              }),
-        },
-        RecipeTutorial:{
-            screen: RecipeTutorial,
-            navigationOptions:{
-                title:"15-minutes recipes"
-            }
-        },
-        InfoPage:{
-            screen: InfoPage,
-            navigationOptions:{
-                title:"About"
-            }
-        },
-        
-    }, {
+                </TouchableOpacity>
+            )
+        }),
+    },
+    RecipeTutorial: {
+        screen: RecipeTutorial,
+        navigationOptions: {
+            title: "15-minutes recipes"
+        }
+    },
+    InfoPage: {
+        screen: InfoPage,
+        navigationOptions: {
+            title: "About"
+        }
+    },
 
-        defaultNavigationOptions: {
-            headerStyle: {
-                backgroundColor: "#9CC02E",
-            },
-            headerTintColor: "#fff",
-            headerTitleAlign: "center",
-            headerBackTitleVisible: false,
-            headerTitleStyle:{
-                fontSize:26
-            }
-        },
-        
-    });
+}, {
 
-    
+    defaultNavigationOptions: {
+        headerStyle: {
+            backgroundColor: "#9CC02E",
+        },
+        headerTintColor: "#fff",
+        headerTitleAlign: "center",
+        headerBackTitleVisible: false,
+        headerTitleStyle: {
+            fontSize: 26
+        }
+    },
+
+});
+
+
 
 export default createAppContainer(Routes);
